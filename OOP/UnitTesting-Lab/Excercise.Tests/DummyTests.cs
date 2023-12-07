fc5ed6e8eac107900882c6d4dc8771570b13aa46
Lyudvika <lydvika@abv.bg>
2023-12-07 22:28:02 +0200

    Adding solved problems from topic: UnitTesting-Lab

---
+++ OOP/UnitTesting-Lab/Excercise.Tests/DummyTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLosesHealthIfAttacked()
+        {
+            Dummy dummy = new Dummy(10, 5);
+
+            dummy.TakeAttack(4);
+
+            Assert.AreEqual(6, dummy.Health);
+        }
+
+        [Test]
+        public void DeadDummyThrowsAnExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(0, 5);
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(4));
+
+            Assert.AreEqual("Dummy is dead.", exception.Message);
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXP()
+        {
+            Dummy dummy = new Dummy(0, 5);
+
+            Assert.AreEqual(5, dummy.GiveExperience());
+        }
+
+        [Test]
+        public void AliveDummyCantGiveXP()
+        {
+            Dummy dummy = new Dummy(10, 5);
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+
+            Assert.AreEqual("Target is not dead.", exception.Message);
+        }
+    }
+}
