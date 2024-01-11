50422a941f9900a69009deefd1755cd6de5f4029
Lyudvika <lydvika@abv.bg>
2024-01-11 18:29:36 +0200

    Adding solved problems from topic: ReflectionAndAttributes-Exercise

---
+++ OOP/ReflectionAndAttributes-Exercise/01.CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,28 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core;
+
+public class CommandInterpreter : ICommandInterpreter
+{
+    public string Read(string args)
+    {
+        string[] commandArg = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        string commandName = commandArg[0];
+
+        Type commandType = Assembly
+            .GetEntryAssembly()
+            .GetTypes()
+            .FirstOrDefault(t => t.Name == $"{commandName}Command");
+
+        if (commandType is null)
+            throw new InvalidOperationException("Command not found");
+
+        ICommand commandInstance = Activator.CreateInstance(commandType) as ICommand;
+
+        return commandInstance.Execute(commandArg.Skip(1).ToArray());
+    }
+}
---
+++ OOP/ReflectionAndAttributes-Exercise/02.ValidationAttributes/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Models;
+
+public class Person
+{
+    private const int MinRange = 12;
+    private const int MaxRange = 90;
+    public Person(string fullName, int age)
+    {
+        FullName = fullName;
+        Age = age;
+    }
+
+    [MyRequired]
+    public string FullName { get; private set; }
+
+    [MyRange(MinRange, MaxRange)]
+    public int Age { get; private set;}
+}
